392db3ff91a9693d7d9b61ae762562668a524441
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-05-12 11:40:58 +0700

    add, edit

--- src/page/Edit/Edit.js
+++ src/page/Edit/Edit.js
@@ -7,18 +7,59 @@ const Edit = () => {
     
     return (
         <div>
-            123456
-
-            {(question || []).map((item, index) => {
-            return (
-              <div className="flex mb-4 items-center" key={index}>
-                <p className="w-full text-grey-darkest">
-                    Add another component to Tailwind Components
-                </p>
+        <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
+          <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
+            <div className="mb-4">
+                <div className = "flex justify-between">
+                <h1 className="text-grey-darkest">Chỉnh sửa câu hỏi</h1>
+              <button className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal">
+                Edit
+              </button>
+                </div>
+              
+              <select className="w-48">
+                <option>...</option>
+                <option>Single choice </option>
+                <option>Multiple choice</option>
+                <option>Single select</option>
+              </select>
+              <div className=" mt-4">
+                <h1>Câu hỏi</h1>
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                  placeholder="thêm câu hỏi"
+                />
               </div>
-            );
-          })}
+              <div className="flex mt-4">
+                <input
+                  type="file"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+                  placeholder="Add Todo"
+                />
+              </div>
+            </div>
+            <div>
+              <h1>Câu trả lời</h1>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 mr-4 text-grey-darker"
+                placeholder="1"
+              />
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 mr-4 text-grey-darker"
+                placeholder="2"
+              />
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 mr-4 text-grey-darker"
+                placeholder="3"
+              />
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 mb-4 mr-4 text-grey-darker"
+                placeholder="4"
+              />
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
--- src/page/Home/Home.js
+++ src/page/Home/Home.js
@@ -12,23 +12,20 @@ function Home(props) {
       console.log(e);
     }
     setShowQuestion();
-  },[]);
+  }, []);
   const deleteNote = async (uid, id) => {
     await questionService.deleteQuestion(uid, id);
   };
   return (
     <div className="h-100 w-full flex items-center justify-center bg-teal-lightest font-sans">
       <div className="bg-white rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg">
-        <div className="mb-4">
+        <div className="mb-4 flex justify-between">
           <h1 className="text-grey-darkest">Danh sách câu hỏi</h1>
-          <div className="flex mt-4">
-            {/* <input className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker" placeholder="Add Todo"/> */}
-            <Link to="/add">
-              <button className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal">
-                Add
-              </button>
-            </Link>
-          </div>
+          <Link to="/add">
+            <button className="flex-no-shrink p-2 border-2 rounded text-teal border-teal hover:text-white hover:bg-teal">
+              Add
+            </button>
+          </Link>
         </div>
         <div>
           {(showQuestion || []).map((item, index) => {
