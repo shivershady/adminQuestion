5d892f284ac1dc4806d3ff01fc42baac2c4100c3
Mien <2502mien@gmail.com>
2022-05-22 11:17:58 +0700

    commit

--- src/page/Question/Edit.js
+++ src/page/Question/Edit.js
@@ -10,11 +10,12 @@ const Edit = () => {
     const [type, setType] = useState(0);
     const [imageQuestion, setImageQuestion] = useState('');
     const [selectedImageQuestion, setSelectedImageQuestion] = useState([]);
-    const [questionText, setQuestionText] = useState("");
+    const [questionText, setQuestionText] = useState('');
     const [error, setError] = useState('');
 
     useEffect(() => {
         _getQuestion();
+    
     }, []);
 
     const _getQuestion = async () => {
@@ -104,7 +105,7 @@ const Edit = () => {
                             className="bg-gray-50 border mt-3 border-gray-300 text-gray-900 mb-6 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             value={type} onChange={(e) => setType(e.target.value)}>
                             <option value="1" key="1">
-                                Single choice{" "}
+                                Single choice
                             </option>
                             <option value="2" key="2">
                                 Multiple choice
